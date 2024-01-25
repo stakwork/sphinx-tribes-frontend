ec15331f75313cfb3dd6a672e4fd4ba480b52ca0
MahtabBukhari <mahtabali50447@gmail.com>
2024-01-25 12:27:45 +0500

    implement the status filter on the org homepage

--- src/people/widgetViews/__tests__/OrgHeader.spec.tsx
+++ src/people/widgetViews/__tests__/OrgHeader.spec.tsx
@@ -1,26 +1,81 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { OrgHeader } from 'pages/tickets/org/orgHeader';
+import { OrgBountyHeaderProps } from '../../interfaces.ts';
+import { mainStore } from '../../../store/main.ts';
+
+const MockProps: OrgBountyHeaderProps = {
+  checkboxIdToSelectedMap: {
+    Open: false,
+    Assigned: false,
+    Paid: false,
+    Completed: false
+  },
+  languageString: '',
+  org_uuid: 'clf6qmo4nncmf23du7ng',
+  onChangeStatus: jest.fn()
+};
 
 describe('OrgHeader Component', () => {
+  beforeEach(() => {
+    jest.spyOn(mainStore, 'getSpecificOrganizationBounties').mockReset();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('renders the component correctly', () => {
-    render(<OrgHeader />);
+    render(<OrgHeader {...MockProps} />);
     expect(screen.getByText('Post a Bounty')).toBeInTheDocument();
-    expect(screen.getByLabelText('Status')).toBeInTheDocument();
+    expect(screen.getByText('Status')).toBeInTheDocument();
     expect(screen.getByLabelText('Skill')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Search')).toBeInTheDocument();
     expect(screen.getByText(/Bounties/i)).toBeInTheDocument();
   });
 
   it('opens the PostModal on "Post a Bounty" button click', async () => {
-    render(<OrgHeader />);
+    render(<OrgHeader {...MockProps} />);
     fireEvent.click(screen.getByText('Post a Bounty'));
   });
 
   it('displays the correct number of bounties', () => {
-    render(<OrgHeader />);
+    render(<OrgHeader {...MockProps} />);
     expect(screen.getByText('284')).toBeInTheDocument();
     expect(screen.getByText('Bounties')).toBeInTheDocument();
+  });
+
+  it('should trigger API call in response to click on status from OrgHeader', async () => {
+    const { getByText, getByRole, rerender } = render(<OrgHeader {...MockProps} />);
+
+    const statusFilter = getByText('Status');
+    expect(statusFilter).toBeInTheDocument();
+    fireEvent.click(statusFilter);
+
+    const statusOpenCheckbox = getByRole('checkbox', { name: /Open/i });
+    expect(statusOpenCheckbox).toBeInTheDocument();
+    fireEvent.click(statusOpenCheckbox);
+
+    await waitFor(() => {
+      expect(MockProps.onChangeStatus).toHaveBeenCalledWith('Open');
+
+      // Simulate a change in checkboxIdToSelectedMap
+      const updatedCheckboxIdToSelectedMap = {
+        ...MockProps.checkboxIdToSelectedMap,
+        Open: true
+      };
+
+      rerender(
+        <OrgHeader {...MockProps} checkboxIdToSelectedMap={updatedCheckboxIdToSelectedMap} />
+      );
+
+      expect(mainStore.getSpecificOrganizationBounties).toHaveBeenCalledWith(MockProps.org_uuid, {
+        page: 1,
+        resetPage: true,
+        ...updatedCheckboxIdToSelectedMap,
+        languageString: MockProps.languageString
+      });
+    });
   });
 });
