a3cc34fc2535e35cf0285ef7cd988321ceb4baf8
ChetanKarwa <cbkarwa@gmail.com>
2024-01-27 01:30:17 +0530

    Bounty List: Fix loaded state when using filters of languages

--- src/people/utils/NoResults.tsx
+++ src/people/utils/NoResults.tsx
@@ -25,11 +25,11 @@ const H = styled.div`
   letter-spacing: 0px;
   color: rgb(60, 63, 65);
 `;
-function NoResults() {
+function NoResults({loaded = false}: {loaded?: boolean}) {
   const { ui } = useStores();
   const { searchText } = ui || {};
-
-  if (searchText) {
+  console.log({searchText})
+  if (searchText || loaded) {
     return (
       <div
         style={{
