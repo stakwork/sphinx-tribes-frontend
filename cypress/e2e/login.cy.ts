2008a07d795c1c2113767f84aac36709bee8b97d
Oluwatobi Bamidele <teebams49@gmail.com>
2024-01-27 00:55:43 +0100

    update: used dynamic user record from nodes array

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,48 +1,60 @@
 describe('User Login', () => {
   it('User gets to login into the app', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Sign in').click();
-
+    let user;
+    const userAlias = 'bob';
     let challenge;
-    let token;
-    let info;
-    cy.get('[data-challenge]')
-      .invoke('attr', 'data-challenge')
-      .then((value) => {
-        const array = value?.split('&');
-        if (array && array.length === 4) {
-          challenge = array[2].substring(10);
+      let token;
+      let info;
+
+    cy.fixture('nodes.json').then((json) => {
+
+      for (let i = 0; i < json.length; i++) {
+        if (json[i].alias === userAlias) {
+          user = json[i];
         }
-      });
-
-    cy.request({
-      method: 'POST',
-      url: 'http://localhost:3001/verify_external',
-      headers: {
-        'x-user-token': 'QycGOV0D7/lcegVqB2BY'
       }
-    }).then((response) => {
-      token = response.body.response.token;
-      info = response.body.response.info;
-    });
+      
+      cy.visit('http://localhost:3000');
+      cy.contains('Sign in').click();
 
-    cy.request({
-      method: 'GET',
-      url: 'http://localhost:3001/signer/U98BoaW54IFZlcmlmaWNhdGlvbg==',
-      headers: {
-        'x-user-token': 'QycGOV0D7/lcegVqB2BY'
-      }
-    }).then((response) => {
-      info.url = 'http://localhost:3001';
-      info['verification_signature'] = response.body.response.sig;
+      cy.get('[data-challenge]')
+        .invoke('attr', 'data-challenge')
+        .then((value) => {
+          const array = value?.split('&');
+          if (array && array.length === 4) {
+            challenge = array[2].substring(10);
+          }
+        });
 
       cy.request({
         method: 'POST',
-        url: `http://localhost:13000/verify/${challenge}?token=${token}`,
-        body: info
-      }).then((response) => {});
-    });
+        url: `${user.external_ip}/verify_external`,
+        headers: {
+          'x-user-token': `${user.authToken}`
+        }
+      }).then((response) => {
+        token = response.body.response.token;
+        info = response.body.response.info;
+      });
 
-    cy.contains("alice").eq(0)
+      cy.request({
+        method: 'GET',
+        url: `${user.external_ip}/signer/U98BoaW54IFZlcmlmaWNhdGlvbg==`,
+        headers: {
+          'x-user-token': `${user.authToken}`
+        }
+      }).then((response) => {
+        info.url = `${user.external_ip}`;
+        info['verification_signature'] = response.body.response.sig;
+
+        cy.request({
+          method: 'POST',
+          url: `http://localhost:13000/verify/${challenge}?token=${token}`,
+          body: info
+        }).then((response) => {});
+      });
+
+      cy.contains(userAlias).eq(0);
+    });
   });
 });
