c5c10534bf634b491d4cb7aab181cac3abf83c84
Oluwatobi Bamidele <teebams49@gmail.com>
2024-01-27 01:10:20 +0100

    feat: added a simple function to help post relay users to tribe server

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -15,6 +15,29 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
+import nodes from '../fixtures/nodes.json';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+async function postAllUsersToTribe() {
+  for (let i = 0; i < nodes.length; i++) {
+    const node = nodes[i];
+    try {
+      await fetch(`${node.external_ip}/profile`, {
+        method: 'POST',
+        body: JSON.stringify({
+          price_to_meet: 0,
+          description: `Testing out how this works by ${node.alias}`,
+          owner_alias: `${node.alias}`,
+          img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR9dAUM-b34F_a6DMw8D6fQ_Y0LUIAVzvfCw&usqp=CAU'
+        }),
+        headers: { 'x-user-token': `${node.authToken}` }
+      });
+    } catch (error) {
+      console.log('This is an error');
+    }
+  }
+}
+
+postAllUsersToTribe();
