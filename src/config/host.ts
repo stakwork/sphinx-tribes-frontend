8bd6e837f7b7ef992e08f25f543a6df19bccf338
Oluwatobi Bamidele <teebams49@gmail.com>
2024-01-27 02:42:58 +0100

    update: change details back to normal

--- src/config/host.ts
+++ src/config/host.ts
@@ -3,7 +3,7 @@ const externalDockerHosts = ['localhost:23007', 'localhost:23000'];
 
 export function getHost(): string {
   const host = window.location.host.includes('localhost')
-    ? 'localhost:13000'
+    ? 'localhost:5002'
     : window.location.host;
   return host;
 }
