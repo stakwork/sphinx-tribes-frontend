dbfa8aa5acf23f4f5b34846eea95cef55c919976
Oluwatobi Bamidele <teebams49@gmail.com>
2024-02-01 23:03:57 +0100

    update: bounty type and run prettier

--- cypress/global.d.ts
+++ cypress/global.d.ts
@@ -1,6 +1,21 @@
 declare namespace Cypress {
   interface Chainable {
     login(userAlias: string): void;
-    logout(userAlias: string): void
+    logout(userAlias: string): void;
   }
+
+  type Bounty = {
+    organization?: string;
+    title: string;
+    github_issue_url?: string;
+    category: string;
+    coding_language?: string[];
+    description: string;
+    amount: number;
+    tribe?: string;
+    estimate_session_length?: string;
+    estimate_completion_date?: string;
+    deliverables?: string;
+    assign?: string;
+  };
 }
