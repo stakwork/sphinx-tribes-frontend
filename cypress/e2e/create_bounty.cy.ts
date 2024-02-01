a5a1851b3e7a9d6ddd11ddb3d6e3411b5ca16e3e
Oluwatobi Bamidele <teebams49@gmail.com>
2024-02-01 01:35:11 +0100

    feat: create a bounty

---
+++ cypress/e2e/create_bounty.cy.ts
@@ -0,0 +1,31 @@
+describe('Alice tries to create a bounty', () => {
+  it("Create a bonty", () => {
+    let activeUser = "alice"
+    cy.login(activeUser)
+    cy.wait(1000)
+
+    cy.contains("Bounties").click()
+    cy.wait(1000)
+    cy.contains("Post a Bounty").click()
+    cy.contains("Start").click()
+    cy.contains("label", "Bounty Title").type("Tobi is testing")
+    cy.get('[data-testid="Category *"]').click()
+    cy.contains("Web development").click()
+    cy.contains( "Coding Language").click()
+    cy.contains("Lightning").click()
+    cy.contains("Typescript").click()
+    cy.contains( "Coding Language").click()
+    cy.contains("Next").click()
+    cy.get('.euiTextArea').type("We are trying to describe what type of bounty we are using currently and hope this works out well for us")
+    cy.contains("Next").click()
+    cy.contains("label", "Price (Sats)").type("1234")
+    cy.contains("Next").click()
+    cy.get('.SearchInput').type("bob")
+    cy.contains("Assign").click()
+    cy.get('.People').contains("Assign").click()
+    cy.contains("Finish").click()
+    
+    cy.wait(1000)
+    cy.logout(activeUser)
+  })
+});
