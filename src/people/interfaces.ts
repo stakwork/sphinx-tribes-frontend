2febbc321006e475994720a1975c780824fb4214
Arrey-Etta <33989423+okhot@users.noreply.github.com>
2024-02-03 17:03:34 +0100

    addresed failing filters

--- src/people/interfaces.ts
+++ src/people/interfaces.ts
@@ -427,6 +427,7 @@ export interface OrgBountyHeaderProps {
   languageString?: string;
   org_uuid?: string;
   organizationData: Organization;
+  direction: string
 }
 export interface PeopleHeaderProps {
   onChangeLanguage: (number) => void;
