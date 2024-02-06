dd04c519ac383e127d2dd03676c086e15302fb1d
Yegor Litvyakov <ylitvyakov@gmail.com>
2024-02-06 16:19:08 +0300

    fix prettier rules

--- src/helpers/helpers-extended.ts
+++ src/helpers/helpers-extended.ts
@@ -364,4 +364,5 @@ const session = [
   }
 ];
 
-export const getSessionValue = (label: string): string => session.find((s) => s.label === label)?.value || '';
+export const getSessionValue = (label: string): string =>
+  session.find((s) => s.label === label)?.value || '';
