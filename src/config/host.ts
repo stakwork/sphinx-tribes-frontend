69cbce9e2b36c56a0c3bc7dfcfacab6cebdc8edb
chiragDork <82438619+chiragDork@users.noreply.github.com>
2024-02-14 19:57:54 +0530

    Update host.ts

--- src/config/host.ts
+++ src/config/host.ts
@@ -7,7 +7,7 @@ export function getHost(): string {
       ? 'localhost:13000'
       : 'localhost:5002'
     : window.location.host;
-  return "community.sphinx.chat";
+  return host;
 }
 
 export function getHostIncludingDockerHosts() {
