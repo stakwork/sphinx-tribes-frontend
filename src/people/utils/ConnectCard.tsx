2045e176bd063be3de65645cfc10e7035f0d3564
Cold Mice <157780212+ColdMice@users.noreply.github.com>
2024-02-19 13:39:57 +0530

    prettier fix

--- src/people/utils/ConnectCard.tsx
+++ src/people/utils/ConnectCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { ConnectCardProps } from 'people/interfaces';
 import { Button, Modal, QR } from '../../components/common';
