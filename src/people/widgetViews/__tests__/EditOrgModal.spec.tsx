b78202bb350d4abf97956db79e379b1c4130eb4c
aliraza556 <alirazaakhtar1122@gmail.com>
2024-02-24 07:31:17 +0500

    fix(character-count): prevent character count increasing if there is no count in the selected field

--- src/people/widgetViews/__tests__/EditOrgModal.spec.tsx
+++ src/people/widgetViews/__tests__/EditOrgModal.spec.tsx
@@ -3,6 +3,14 @@ import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { Organization } from 'store/main';
 import EditOrgModal from '../organization/EditOrgModal';
+import { useStores } from '../../../store';
+
+jest.mock('../../../store', () => ({
+  useStores: jest.fn()
+}));
+
+const mockUpdateOrganization = jest.fn();
+const mockAddToast = jest.fn();
 
 const mockOrganization: Organization = {
   id: '1',
@@ -29,6 +37,20 @@ const props = {
   addToast: () => null,
   close: () => null
 };
+
+beforeEach(() => {
+  mockUpdateOrganization.mockReset();
+  mockAddToast.mockReset();
+
+  (useStores as jest.Mock).mockReturnValue({
+    main: {
+      updateOrganization: mockUpdateOrganization
+    },
+    ui: {
+      meInfo: { owner_pubkey: 'xyz456' }
+    }
+  });
+});
 
 describe('EditOrgModal Component', () => {
   test('displays the Organization Name text field', () => {
@@ -85,5 +107,37 @@ describe('EditOrgModal Component', () => {
       });
       expect(screen.getByText(/Save changes/i)).toBeEnabled();
     });
+  });
+
+  test('Entering text in GitHub field does not increase character count in description field', async () => {
+    render(<EditOrgModal {...props} />);
+
+    const initialDescriptionElement = screen.getByLabelText(/Description/i) as HTMLInputElement;
+    const initialDescriptionLength = initialDescriptionElement.value.length;
+
+    fireEvent.change(screen.getByLabelText(/Github repo/i) as HTMLInputElement, {
+      target: { value: 'https://github.com/newRepo_testing_of_the_bounty' }
+    });
+
+    const updatedDescriptionElement = screen.getByLabelText(/Description/i) as HTMLInputElement;
+    const updatedDescriptionLength = updatedDescriptionElement.value.length;
+
+    expect(updatedDescriptionLength).toBe(initialDescriptionLength);
+  });
+
+  test('Entering text in Website field does not increase character count in description field', async () => {
+    render(<EditOrgModal {...props} />);
+
+    const initialDescriptionElement = screen.getByLabelText(/Description/i) as HTMLInputElement;
+    const initialDescriptionLength = initialDescriptionElement.value.length;
+
+    fireEvent.change(screen.getByLabelText(/Website/i) as HTMLInputElement, {
+      target: { value: 'https://test.org_the_sphinx_second_brain' }
+    });
+
+    const updatedDescriptionElement = screen.getByLabelText(/Description/i) as HTMLInputElement;
+    const updatedDescriptionLength = updatedDescriptionElement.value.length;
+
+    expect(updatedDescriptionLength).toBe(initialDescriptionLength);
   });
 });
