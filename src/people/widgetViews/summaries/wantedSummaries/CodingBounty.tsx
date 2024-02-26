d4ffe37874e58fa6fd82db51804f77389c7dea93
Vayras <89837102+Vayras@users.noreply.github.com>
2024-02-26 17:55:37 +0530

    delete button fix:

--- src/people/widgetViews/summaries/wantedSummaries/CodingBounty.tsx
+++ src/people/widgetViews/summaries/wantedSummaries/CodingBounty.tsx
@@ -110,6 +110,7 @@ function MobileView(props: CodingBountiesProps) {
   const [toasts, setToasts]: any = useState([]);
   const [updatingPayment, setUpdatingPayment] = useState<boolean>(false);
   const [userBountyRole, setUserBountyRole] = useState(false);
+  const [enableDelete,setEnableDelete] = useState(false);
 
   const [paidStatus, setPaidStatus] = useState(paid);
 
@@ -567,7 +568,7 @@ function MobileView(props: CodingBountiesProps) {
                             leadingImageContainerStyle={{
                               left: 450
                             }}
-                            disabled={!props?.deleteAction}
+                            disabled={enableDelete}
                             buttonAction={props?.deleteAction}
                             buttonTextStyle={{
                               paddingRight: '45px'
@@ -662,6 +663,7 @@ function MobileView(props: CodingBountiesProps) {
                             className="AssigneeCloseButtonContainer"
                             onClick={() => {
                               changeAssignedPerson();
+                              setEnableDelete(false)
                             }}
                           >
                             <img
@@ -695,6 +697,7 @@ function MobileView(props: CodingBountiesProps) {
                           }}
                           buttonAction={() => {
                             assigneeHandlerOpen();
+                            setEnableDelete(true)
                           }}
                         />
                       </div>
