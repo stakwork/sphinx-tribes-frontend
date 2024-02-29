d46936bfd8f1137b24a7a6f3d27170f5c2213ce3
Vayras <89837102+Vayras@users.noreply.github.com>
2024-02-29 11:46:33 +0530

    test id

--- src/people/widgetViews/summaries/wantedSummaries/CodingBounty.tsx
+++ src/people/widgetViews/summaries/wantedSummaries/CodingBounty.tsx
@@ -559,6 +559,7 @@ function MobileView(props: CodingBountiesProps) {
                             disabled={isEditButtonDisable}
                           />
                           <ImageButton
+                            dataTestId="delete-btn"
                             buttonText={!props.deletingState ? 'Delete' : 'Deleting'}
                             ButtonContainerStyle={{
                               width: '117px',
