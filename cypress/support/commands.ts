ca7b3bf5b826f9456d9cf2ec29f541270d05aa82
chiragDork <vyaschirag92@gmail.com>
2024-03-13 12:05:36 +0530

    prettier changes

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -268,13 +268,14 @@ Cypress.Commands.add('create_org', (organization) => {
 });
 
 Cypress.Commands.add('assert_org_bounty', (bounty, organization) => {
+  cy.wait(5000);
+  cy.contains('label', 'Bounty Title')
+    .type(bounty.title)
+    .should('exist')
+    .and('contain', organization);
 
-  cy.wait(5000); 
-  cy.contains('label', 'Bounty Title').type(bounty.title)
-  .should('exist')
-  .and('contain', organization);
-
-  cy.contains('label', 'Bounty Title').type(bounty.title)
+  cy.contains('label', 'Bounty Title')
+    .type(bounty.title)
     .find('button[data-testid="Organization"]')
     .click();
 
