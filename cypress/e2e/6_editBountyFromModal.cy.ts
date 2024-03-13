bbda274b7c16a7b3f7e1b6bc7696885c7dd56c70
Vayras <89837102+Vayras@users.noreply.github.com>
2024-03-13 22:19:14 +0530

    fix

--- cypress/e2e/6_editBountyFromModal.cy.ts
+++ cypress/e2e/6_editBountyFromModal.cy.ts
@@ -13,22 +13,16 @@ describe('Edit Bounty From Modal', () => {
       estimate_completion_date: '09/09/2024'
     });
 
-    // Open the first bounty on the page
     cy.contains('My new Bounty').click();
-
-    // Click on the edit button
     cy.contains('Edit').click();
 
-    // Edit the bounty title and description
     const newTitle = 'Edited Bounty Title';
     const newDescription = 'This bounty has been edited';
-    cy.get('[data-testid="EditTitle"]').clear().type(newTitle);
-    cy.get('[data-testid="EditDescription"]').clear().type(newDescription);
 
-    // Save the changes
+    cy.get('[data-testid="one_sentence_summary"]').clear().type(newTitle);
+    cy.get('[data-testid="description"]').clear().type(newDescription);
+
     cy.contains('Save Changes').click();
-
-    // Assert that the new bounty title and description match what was edited
     cy.contains(newTitle).should('exist');
     cy.contains(newDescription).should('exist');
   });
