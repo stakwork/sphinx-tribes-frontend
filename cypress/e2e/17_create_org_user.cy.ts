d6cc33814953310b565f4170a7918c100de0809b
Vayras <89837102+Vayras@users.noreply.github.com>
2024-03-16 01:39:42 +0530

    test

--- cypress/e2e/17_create_org_user.cy.ts
+++ cypress/e2e/17_create_org_user.cy.ts
@@ -1,8 +1,8 @@
 describe('Add User to Organization', () => {
   it('should add a user to an organization', () => {
     const org = {
-      loggedInAs: 'carol',
-      name: 'Org1A',
+      loggedInAs: 'bob',
+      name: 'NEW ORG TO TEST USER ROLES',
       description: 'We are testing out our organization',
       website: 'https://community.sphinx.chat',
       github: 'https://github.com/stakwork/sphinx-tribes-frontend'
@@ -14,7 +14,7 @@ describe('Add User to Organization', () => {
     cy.create_org(org);
     cy.wait(1000);
 
-    cy.contains('Manage').click();
+    cy.contains(org.name).contains('Manage').click();
     cy.wait(1000);
 
     cy.contains('Add User').click();
@@ -30,18 +30,16 @@ describe('Add User to Organization', () => {
 
     cy.get('#sphinx-top-level-overlay').within(() => {
       cy.contains('Add User').click();
+      cy.wait(1000);
+    });
+    cy.wait(1000);
+
+    cy.get('#sphinx-top-level-overlay').within(() => {
+      cy.contains('Assign').click();
+      cy.wait(1000);
     });
 
-    cy.get('#Manage bounties').should('be.checked');
-    cy.get('#Fund organization').should('be.checked');
-    cy.get('#Withdraw from organization').should('be.checked');
-    cy.get('#View transaction history').should('be.checked');
-
-    cy.contains('Assign').click();
     cy.wait(1000);
-
-    cy.wait(1000);
-
     cy.logout(org.loggedInAs);
   });
 });
