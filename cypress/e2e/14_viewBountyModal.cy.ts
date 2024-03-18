3d1c315e2864fd764ddfa2956b801f8bd188ffc0
Arrey-Etta <arreyetta1jnr@gmail.com>
2024-03-18 23:02:47 +0100

    fix cypress test

--- cypress/e2e/14_viewBountyModal.cy.ts
+++ cypress/e2e/14_viewBountyModal.cy.ts
@@ -1,58 +1,54 @@
-describe('View bounty modal', () => {
-  const assignee = 'carol';
+describe('View Organization Bounties', () => {
+  const OrgName = 'OrganizationView';
+
+  const org: Cypress.Organization = {
+    loggedInAs: 'alice',
+    name: OrgName,
+    description: 'An organization focused on amazing projects.',
+    website: 'https://amazing.org',
+    github: 'https://github.com/amazing'
+  };
+
   const bounty: Cypress.Bounty = {
-    title: 'My new Bounty',
+    organization: OrgName,
+    title: 'Bounty',
     category: 'Web development',
     coding_language: ['Typescript', 'Javascript', 'Lightning'],
     description: 'This is available',
     amount: '123',
-    assign: assignee,
-    deliverables: 'We are good to go man',
-    tribe: '',
+    tribe: 'Amazing Org Tribe',
     estimate_session_length: 'Less than 3 hour',
-    estimate_completion_date: '09/09/2024'
+    estimate_completion_date: '09/09/2024',
+    deliverables: 'We are good to go man',
+    assign: ''
   };
 
-  it('Can edit a bounty from modal', () => {
-    const activeUser = 'alice';
-    cy.login(activeUser);
-    cy.wait(5000);
+  beforeEach(() => {
+    cy.login(org.loggedInAs);
+    cy.wait(1000);
+    cy.create_org(org);
+    cy.wait(1000);
+  });
 
+  it('should verify  bounty tile displays the organization label is clickable and redirects the user to the organizations bounty page.', () => {
     cy.create_bounty(bounty);
     cy.wait(5000);
 
-    cy.contains(bounty.title).click();
+    cy.contains(bounty.title);
     cy.wait(5000);
 
-
-    cy.contains(activeUser);
+    cy.contains(org.name);
     cy.wait(1000);
 
-    cy.contains(bounty.title);
+    cy.contains(org.name).invoke('removeAttr', 'target').click({ force: true });
+    cy.wait(5000);
+
+    cy.url().should('include', '/org/bounties');
+    cy.wait(5000);
+
+    cy.contains(bounty.title).should('exist');
     cy.wait(1000);
 
-    cy.contains(bounty.assign);
-    cy.wait(1000);
-
-    cy.contains(bounty.amount);
-    cy.wait(1000);
-
-    cy.contains(bounty.estimate_completion_date);
-    cy.wait(1000);
-
-    cy.contains(bounty.deliverables);
-    cy.wait(1000);
-
-    cy.contains(bounty.description);
-    cy.wait(1000);
-
-    cy.contains('Mark as Paid');
-    cy.wait(1000);
-
-    cy.contains('Copy Link');
-    cy.wait(1000);
-
-    cy.contains('Share to Twitter');
-    cy.wait(1000);
+    cy.logout(org.loggedInAs);
   });
 });
