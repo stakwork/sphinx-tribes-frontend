0cf1918c62934c1b7c2e9d9b1e24f03cf51e6ae2
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-03-18 22:12:01 +0530

    Update 17_create_org_user.cy.ts

--- cypress/e2e/17_create_org_user.cy.ts
+++ cypress/e2e/17_create_org_user.cy.ts
@@ -1,8 +1,8 @@
 describe('Add User to Organization', () => {
   it('should add a user to an organization', () => {
     const org = {
-      loggedInAs: 'alice',
-      name: 'Budget Org 2',
+      loggedInAs: 'carol',
+      name: 'TEST E2E',
       description: 'We are testing out our organization',
       website: 'https://community.sphinx.chat',
       github: 'https://github.com/stakwork/sphinx-tribes-frontend'
@@ -20,7 +20,7 @@ describe('Add User to Organization', () => {
     cy.contains('Add User').click();
     cy.wait(1000);
 
-    cy.get('input').type('bob');
+    cy.get('input').type('alice');
     cy.wait(1000);
 
     cy.contains('Select').click();
@@ -38,6 +38,28 @@ describe('Add User to Organization', () => {
       cy.contains('Assign').click();
       cy.wait(1000);
     });
+
+    cy.contains('alice').get('[data-testid="settings-icon"]').click();
+    cy.wait(1000);
+
+    cy.contains('label', 'Manage bounties').prev('input[type="checkbox"]').should('be.checked');
+    cy.wait(1000);
+
+    cy.contains('label', 'Fund organization').prev('input[type="checkbox"]').should('be.checked');
+    cy.wait(1000);
+
+    cy.contains('label', 'Withdraw from organization')
+      .prev('input[type="checkbox"]')
+      .should('be.checked');
+    cy.wait(1000);
+
+    cy.contains('label', 'View transaction history')
+      .prev('input[type="checkbox"]')
+      .should('be.checked');
+    cy.wait(1000);
+
+    cy.contains('Update roles').click();
+    cy.wait(1000);
 
     cy.wait(1000);
     cy.logout(org.loggedInAs);
