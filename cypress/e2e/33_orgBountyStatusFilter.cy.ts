0eab9171f6ea05de3b081fe84986a64b6c003e37
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-03-20 17:46:28 +0530

    Update 33_orgBountyStatusFilter.cy.ts

--- cypress/e2e/33_orgBountyStatusFilter.cy.ts
+++ cypress/e2e/33_orgBountyStatusFilter.cy.ts
@@ -1,6 +1,6 @@
 describe('filter by status for org bounty', () => {
   it('should filter bounties according to the status selected', () => {
-    const OrgName = 'E2EOrg';
+    const OrgName = 'E2EOrg2';
 
     const org: Cypress.Organization = {
       loggedInAs: 'bob',
