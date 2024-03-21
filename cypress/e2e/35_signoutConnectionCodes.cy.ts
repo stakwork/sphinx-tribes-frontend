94febee2e4ffe4862cc9b1306d9cc3b1c958ac48
chiragDork <82438619+chiragDork@users.noreply.github.com>
2024-03-21 21:26:27 +0530

    Update 35_signoutConnectionCodes.cy.ts

--- cypress/e2e/35_signoutConnectionCodes.cy.ts
+++ cypress/e2e/35_signoutConnectionCodes.cy.ts
@@ -1,6 +1,6 @@
 describe('Sign Out Connection Codes', () => {
   it('should display a message when a user tries to post a bounty without signing in', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:3007');
     cy.wait(1000);
 
     cy.contains('Bounties').click();
