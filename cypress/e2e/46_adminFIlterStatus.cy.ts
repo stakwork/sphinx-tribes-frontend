a3d0d451b63fe7e78684ccb12cd647f4fd6b7a1f
MahtabBukhari <mahtabali50447@gmail.com>
2024-03-27 19:07:19 +0500

    super admin bounty filter status dropdown

---
+++ cypress/e2e/46_adminFIlterStatus.cy.ts
@@ -0,0 +1,93 @@
+describe('Super Admin Bounty Filter Status Dropdown ', () => {
+  let activeUser = 'alice';
+
+  const bounty: Cypress.Bounty = {
+    title: 'SyedAli',
+    category: 'Web development',
+    description: 'This is available',
+    amount: '123',
+    assign: 'carol',
+    deliverables: 'We are good to go man'
+  };
+
+  beforeEach(() => {
+    cy.login(activeUser);
+    cy.wait(1000);
+  });
+
+  it('Should create six bounties with different status and verify that changing the bounties filter status displays only bounties with that specific status', () => {
+    for (let i = 1; i <= 4; i++) {
+      const updatedBounty = { ...bounty, title: `SyedAli${i}` };
+      cy.create_bounty(updatedBounty);
+      cy.wait(1000);
+    }
+
+    for (let i = 5; i <= 6; i++) {
+      const updatedBounty = { ...bounty, title: `SyedAli${i}`, assign: '' };
+      cy.create_bounty(updatedBounty);
+      cy.wait(1000);
+    }
+
+    for (let i = 1; i <= 2; i++) {
+      cy.contains('Filter').click();
+      cy.contains('Assigned').click();
+      cy.wait(1000);
+
+      cy.contains(`SyedAli${i}`).click();
+      cy.wait(1000);
+
+      cy.contains('Mark as Paid').click();
+      cy.wait(1000);
+
+      cy.contains('Next').click();
+      cy.wait(1000);
+
+      cy.contains('Skip and Mark Paid').click();
+      cy.wait(1000);
+
+      cy.get('body').click(0, 0);
+
+      cy.wait(1000);
+    }
+
+    cy.visit('http://localhost:3007/admin');
+    cy.wait(3000);
+
+    for (let i = 1; i <= 6; i++) {
+      cy.contains(`SyedAli${i}`, { timeout: 10000 }).should('exist');
+    }
+
+    cy.contains('Status').click();
+    cy.get('label[for="Open"]').click();
+    cy.contains('Apply').click();
+    cy.wait(1000);
+
+    for (let i = 5; i <= 6; i++) {
+      cy.contains(`SyedAli${i}`, { timeout: 10000 }).should('exist');
+    }
+
+    cy.contains('Status').click();
+    cy.get('label[for="Open"]').click();
+    cy.get('label[for="Assigned"]').click();
+    cy.contains('Apply').click();
+    cy.wait(1000);
+
+    for (let i = 3; i <= 4; i++) {
+      cy.contains(`SyedAli${i}`, { timeout: 10000 }).should('exist');
+    }
+
+    cy.contains('Status').click();
+    cy.get('label[for="Assigned"]').click();
+    cy.get('label[for="Paid"]').click();
+    cy.contains('Apply').click();
+    cy.wait(1000);
+
+    for (let i = 1; i <= 2; i++) {
+      cy.contains(`SyedAli${i}`, { timeout: 10000 }).should('exist');
+    }
+
+    cy.wait(1000);
+
+    cy.logout(activeUser);
+  });
+});
