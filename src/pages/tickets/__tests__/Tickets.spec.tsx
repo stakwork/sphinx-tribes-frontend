a181c43dec2c2935d166dd965678edd6f5f9d63d
aliraza556 <alirazaakhtar1122@gmail.com>
2024-04-05 15:22:14 +0500

    add unit test tickets

--- src/pages/tickets/__tests__/Tickets.spec.tsx
+++ src/pages/tickets/__tests__/Tickets.spec.tsx
@@ -297,6 +297,20 @@ describe('Tickets Component', () => {
     })();
   });
 
+  it('Test that a newly created bounty is visible.', async () => {
+    render(<Tickets />);
+
+    (async () => {
+      await waitFor(() => {
+        expect(screen.queryByTestId('tickets-component')).toBeInTheDocument();
+      });
+
+      mockBounties.forEach(({ bounty }) => {
+        expect(screen.getByText(`bounty title: ${bounty.title}`)).toBeInTheDocument();
+      });
+    })();
+  });
+
   it('tests that out of connection code is displayed when there are no codes', () => {
     uiStore.setMeInfo(null);
 
