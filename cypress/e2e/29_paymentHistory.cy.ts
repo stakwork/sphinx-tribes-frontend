548a67509016e40fd622c602dcd4cc2dd0ef319f
elraphty <elraphtyofficial@gmail.com>
2024-05-10 09:30:03 +0100

    changed org name

--- cypress/e2e/29_paymentHistory.cy.ts
+++ cypress/e2e/29_paymentHistory.cy.ts
@@ -9,7 +9,7 @@ describe('It Lists all payments in history', () => {
     // create workspace
     const workSpace = {
       loggedInAs: activeUser,
-      name: 'Test Workspace',
+      name: 'Payment Flow',
       description: 'We are testing out our workspace',
       website: 'https://community.sphinx.chat',
       github: 'https://github.com/stakwork/sphinx-tribes-frontend'
