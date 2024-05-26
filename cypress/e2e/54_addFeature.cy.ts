439a34fb3ffd0f556dfff4d30f8b82f4acaf3e59
AbdulWahab3181 <abdulwahab3181@gmail.com>
2024-05-27 03:59:21 +0500

    commented add feature cypress test

--- cypress/e2e/54_addFeature.cy.ts
+++ cypress/e2e/54_addFeature.cy.ts
@@ -1,49 +1,49 @@
-describe('Add feature To Workspace', () => {
-  it('Creating A Workspace', () => {
-    cy.login('carol');
-    cy.wait(1000);
-
-    const WorkSpaceName = 'Workspace Feature';
-
-    const workspace = {
-      loggedInAs: 'carol',
-      name: WorkSpaceName,
-      description: 'We are testing out our workspace feature',
-      website: 'https://community.sphinx.chat',
-      github: 'https://github.com/stakwork/sphinx-tribes-frontend'
-    };
-
-    cy.create_workspace(workspace);
-    cy.wait(1000);
-
-    cy.contains(workspace.name).contains('Manage').click();
-    cy.wait(1000);
-
-    cy.get('[data-testid="mission-link"]')
-      .invoke('show')
-      .then(($link: JQuery<HTMLElement>) => {
-        const modifiedHref = $link.attr('href');
-        cy.wrap($link).invoke('removeAttr', 'target');
-        cy.wrap($link).click();
-        cy.url().should('include', modifiedHref);
-      });
-    cy.wait(1000);
-
-    cy.contains('No mission yet');
-    cy.contains('No tactics yet');
-
-    cy.get('[data-testid="new-feature-btn"]').click();
-    cy.wait(1000);
-
-    cy.contains('Add New Feature');
-
-    const newFeature = 'This is my Feature';
-    cy.get('[data-testid="feature-input"]').type(newFeature);
-    cy.get('[data-testid="add-feature-btn"]').click();
-    cy.wait(1000);
-
-    cy.contains(newFeature).should('exist', { timeout: 3000 });
-
-    cy.logout('carol');
-  });
-});
+// describe('Add feature To Workspace', () => {
+//   it('Creating A Workspace', () => {
+//     cy.login('carol');
+//     cy.wait(1000);
+//
+//     const WorkSpaceName = 'Workspace Feature';
+//
+//     const workspace = {
+//       loggedInAs: 'carol',
+//       name: WorkSpaceName,
+//       description: 'We are testing out our workspace feature',
+//       website: 'https://community.sphinx.chat',
+//       github: 'https://github.com/stakwork/sphinx-tribes-frontend'
+//     };
+//
+//     cy.create_workspace(workspace);
+//     cy.wait(1000);
+//
+//     cy.contains(workspace.name).contains('Manage').click();
+//     cy.wait(1000);
+//
+//     cy.get('[data-testid="mission-link"]')
+//       .invoke('show')
+//       .then(($link: JQuery<HTMLElement>) => {
+//         const modifiedHref = $link.attr('href');
+//         cy.wrap($link).invoke('removeAttr', 'target');
+//         cy.wrap($link).click();
+//         cy.url().should('include', modifiedHref);
+//       });
+//     cy.wait(1000);
+//
+//     cy.contains('No mission yet');
+//     cy.contains('No tactics yet');
+//
+//     cy.get('[data-testid="new-feature-btn"]').click();
+//     cy.wait(1000);
+//
+//     cy.contains('Add New Feature');
+//
+//     const newFeature = 'This is my Feature';
+//     cy.get('[data-testid="feature-input"]').type(newFeature);
+//     cy.get('[data-testid="add-feature-btn"]').click();
+//     cy.wait(1000);
+//
+//     cy.contains(newFeature).should('exist', { timeout: 3000 });
+//
+//     cy.logout('carol');
+//   });
+// });
