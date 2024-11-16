6c792094d9ba0aca669273a82895da07f9d60f74
MahtabBukhari <mahtabali50447@gmail.com>
2024-11-16 14:27:49 +0500

    update cypress test 0

--- cypress/e2e/0_createBounty.cy.ts
+++ cypress/e2e/0_createBounty.cy.ts
@@ -1,35 +1,39 @@
 describe('Alice tries to create a bounty', () => {
+  const workspace: Cypress.Workspace = {
+    loggedInAs: 'alice',
+    name: 'Workspace1',
+    description: 'A workspace focused on amazing projects.',
+    website: 'https://amazing.org',
+    github: 'https://github.com/amazing'
+  };
+
+  const bounty: Cypress.Bounty = {
+    workspace:'Workspace1',
+    title: 'My new Bounty',
+    category: 'Web development',
+    coding_language: ['Typescript', 'Javascript', 'Lightning'],
+    description: 'This is available',
+    amount: '123',
+    assign: 'carol',
+    deliverables: 'We are good to go man',
+    tribe: '',
+    estimate_session_length: 'Less than 3 hour',
+    estimate_completion_date: '09/09/2024'
+  };
+
+  beforeEach(() => {
+    cy.login(workspace.loggedInAs);
+    cy.wait(1000);
+    cy.create_workspace(workspace);
+    cy.wait(1000);
+  });
+
   it('Create a bounty', () => {
-    let activeUser = 'alice';
-    cy.login(activeUser);
-    cy.wait(1000);
 
-    cy.create_workspace({
-      loggedInAs: activeUser,
-      name: 'workspace1',
-      description: 'We are testing out our workspace',
-      website: 'https://community.sphinx.chat',
-      github: 'https://github.com/stakwork/sphinx-tribes-frontend'
-    });
+    cy.create_bounty(bounty);
 
     cy.wait(1000);
 
-    cy.create_bounty({
-      title: 'My new Bounty',
-      workspace:'workspace1',
-      category: 'Web development',
-      coding_language: ['Typescript', 'Javascript', 'Lightning'],
-      description: 'This is available',
-      amount: '123',
-      assign: 'carol',
-      deliverables: 'We are good to go man',
-      tribe: '',
-      estimate_session_length: 'Less than 3 hour',
-      estimate_completion_date: '09/09/2024'
-    });
-
-    cy.wait(1000);
-
-    cy.logout(activeUser);
+    cy.logout(workspace.loggedInAs);
   });
 });
