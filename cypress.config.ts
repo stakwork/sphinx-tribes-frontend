2205585e6128a170553f53b60d3f53cce2c01f67
Oluwatobi Bamidele <teebams49@gmail.com>
2024-12-10 17:26:39 +0300

    chore: set video recording as true

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   viewportWidth: 1800,
   viewportHeight: 1200,
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
