f22367a8f59bee51fb74d190f2f4030e32ed86f2
aliraza556 <alirazaakhtar1122@gmail.com>
2025-01-04 05:19:20 +0500

    fix cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   viewportWidth: 1800,
   viewportHeight: 1200,
   video: true,
   e2e: {
+    retries: {
+      runMode: 3,
+      openMode: 1
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
+    }
+  }
 });
