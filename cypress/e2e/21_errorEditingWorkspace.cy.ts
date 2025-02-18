a11d22e94487ba062a53774cc6d84bd7c10b8546
saithsab877 <saithsab877@gmail.com>
2025-02-19 02:07:12 +0500

    fix 21_workspaceError v3

--- cypress/e2e/21_errorEditingWorkspace.cy.ts
+++ cypress/e2e/21_errorEditingWorkspace.cy.ts
@@ -11,12 +11,10 @@ describe('Alice Create a Workspace and then Edit to validate characters the limi
       'This description is intentionally made longer than one hundred and twenty characters to test the validation functionality of the workspace creation form.'
   };
 
-  before(() => {
+  it('should not allow editing a workspace with excessive character limits', () => {
     cy.login(worksapce.loggedInAs);
     cy.wait(1000);
-  });
 
-  it('should not allow editing a workspace with excessive character limits', () => {
     cy.clickAlias(worksapce.loggedInAs);
     cy.wait(1000);
 
