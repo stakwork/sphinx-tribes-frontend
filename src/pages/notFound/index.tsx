377a14fcaa8490b11eb8fad73470df9a01ed89c7
Ekep Obasi <arreyettaekep@gmail.com>
2025-03-13 21:43:13 -0400

    feat: add 404 page with responsive design

---
+++ src/pages/notFound/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+  NotFoundPageContainer,
+  ContentWrapper,
+  TextContent,
+  Message,
+  ImageContainer,
+  ErrorCode,
+  Button,
+  Title,
+  Image
+} from './styles';
+
+export function NotFoundPage() {
+  const history = useHistory();
+
+  return (
+    <NotFoundPageContainer>
+      <ContentWrapper>
+        <TextContent>
+          <div>
+            <ErrorCode>404</ErrorCode>
+            <Title>Looks like you&apos;ve found the doorway to the great nothing!</Title>
+            <Message>
+              The page you&apos;re looking for doesn&apos;t exist or has been moved.
+            </Message>
+            <Button onClick={() => history.push('/')}>Take me back</Button>
+          </div>
+        </TextContent>
+        <ImageContainer>
+          <Image src="/error.png" alt="/error.png" />
+        </ImageContainer>
+      </ContentWrapper>
+    </NotFoundPageContainer>
+  );
+}
