2e5d53c381e757f55a478f154768bbc30da29714
aliraza556 <alirazaakhtar1122@gmail.com>
2025-03-14 05:55:45 +0500

    fix-unit-test

---  jest.config.js
+++  jest.config.js
@@ -23,5 +23,9 @@ module.exports = {
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
 
     // Module file extensions for importing
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
-};
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    
+    moduleNameMapper: {
+        "\\.(css|less|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.js"
+    }
+}; 
